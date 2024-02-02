3c7ac571513041e45af72dbb6c5e610d875b0dc4
jay suthar <tirth4134@gmail.com>
2024-02-02 11:19:54 +0530

    # sanity Done #

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import pin from './pin'
+import postedBy from './postedBy'
+import comment from './comment'
+import save from './save'
+import user from './user'
+
+export const schemaTypes = [user, pin, comment, postedBy, save]
---
+++ schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'usename',
+      title: 'Username',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
